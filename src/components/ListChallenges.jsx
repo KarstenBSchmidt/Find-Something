6aab0333ede511ab7b3ddae05c9c70d2655b882f
Karsten Schmidt <kbskjs1@gmail.com>
2025-03-12 13:27:15 -0700

    got challenges to load giving back data

--- src/components/ListChallenges.jsx
+++ src/components/ListChallenges.jsx
@@ -1,22 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { getChallenges } from '../firestore';
 
 export default function ListChallenges() {
-    
+
+  const [challenges, setChallenges] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getChallenges()
+      .then(data => setChallenges(data))
+      .catch(err => setError(err));
+  }, []);
+
   return (
     <>
-    <h1>Previous Challenges</h1>
-    <ul>
-        {/*(getChallenges()).map((challenge, index) => (
+      <h1>Previous Challenges</h1>
+      {error ? (
+        <p>Error loading challenges: {error.message}</p>
+      ) : (
+        <ul>
+          {challenges.map((challenge, index) => (
             <li key={index}>
-                <p>Longitude: {challenge.longitude}</p>
-                <p>Latitude: {challenge.latitude}</p>
-                <p>Distance: {challenge.distance}</p>
-                <p>Created At: {challenge.createdAt}</p>
+              <p>Longitude: {challenge.longitude}</p>
+              <p>Latitude: {challenge.latitude}</p>
+              <p>Distance: {challenge.distance}</p>
+              <p>Created At: {challenge.createdAt}</p>
             </li>
-        ))*/}
-    </ul>
+          ))}
+        </ul>
+      )}
     </>
-    
-  )
+  );
 }
